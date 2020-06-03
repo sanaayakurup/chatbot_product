954b81ba738c9020d560cec8081d722e4d76efd3
sanaayakurup <sanaayakurup@gmail.com>
2020-06-03 16:15:02 +0530

    added verify user api, without sql commands

--- src/app.py
+++ src/app.py
@@ -35,25 +35,43 @@ logger.info("Begin logging. . .") #use this line of code to log in specific erro
 
 #attributes for userdata class
 class UI_UserData():
-    def __init__(self, user_name, user_number):
+    def __init__(self, user_name, user_number, otp):
         self.user_name=user_name
         self.user_number=user_number 
+        self.otp=otp
         
 #register-user api 
 @application.route("/user-registration/<user_name>/<user_number>", methods=["POST"])
 def run_insert_doa(user_name, user_number):
-    userdata_obj=UI_UserData(user_name,user_number) #create an instance of the class with the vars coming from the UI
     #making a connection to the db 
     conn = Connection.create_conn_instance(myConfig)
     cursor = conn.cursor
     #gen otp
     user_otp=random_with_N_digits(3) #user_otp generated
-    #calls gagans code which inserts in the table 
+    userdata_obj=UI_UserData(user_name,user_number,user_otp)
+    #calls gagans code which inserts in the table
     UserData.CreateData('user_data', userdata_obj.user_name, userdata_obj.user_number, user_otp, "0", conn, cursor)
-   # UserData.ReadData('user_data',conn,  cursor)
     logger.info("User Details inserted into database")
     return json.dumps({"response_code":"200","message":"successfuly entered data in db"})
+
+@application.route("/verify-user/<otp>", methods=["GET"])
+def verify_user(otp):
+    #making a connection to the db 
+    conn = Connection.create_conn_instance(myConfig)
+    cursor = conn.cursor
+    user_provided_otp=otp #save users supplied otp in otp var #UserData.ReadData('user_data',conn,  cursor)
+    generated_otp=3652 #change this to a select sql where user_number==, fetch this val from a table 
+    if generated_otp==user_provided_otp:
+        print("your account is verified, you can carry on")
+        logger.info("Account was verifed")
+
+        #run update in table where user_number==something
+    else:
+        print("please provide the correct otp")
+        logger.info("Account was not verifed")
+   
     
+
 if __name__ == '__main__':
     application.run( port = 5000, host='127.0.0.1')
     
